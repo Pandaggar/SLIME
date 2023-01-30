52e55909e9940ec5a83188198491fe83514d31d8
Pandaggar <Pandag3006@gmail.com>
2023-01-30 00:20:19 -0500

    dont work but existst it do
    bad

---
+++ src/defines.h
@@ -0,0 +1,19 @@
+#define IMU IMU_MPU6050
+#define SECOND_IMU IMU
+#define BOARD BOARD_WROOM32
+#define IMU_ROTATION DEG_180
+#define SECOND_IMU_ROTATION DEG_180
+
+// Battery monitoring options (comment to disable):
+// BAT_EXTERNAL for ADC pin, BAT_INTERNAL for internal - can detect only low battery, BAT_MCP3021 for external ADC
+#define BATTERY_MONITOR BAT_EXTERNAL
+#define BATTERY_SHIELD_RESISTANCE 180 //130k BatteryShield, 180k SlimeVR or fill in external resistor value in kOhm
+
+#define PIN_IMU_SDA 21
+#define PIN_IMU_SCL 22
+#define PIN_IMU_INT 23
+#define PIN_IMU_INT_2 25
+#define PIN_BATTERY_LEVEL 36
+#define POT_1 39
+#define POT_2 36
+#define POT_3 35
